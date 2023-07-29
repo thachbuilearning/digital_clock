10a3120842d828ba60a3ca13f703ac803fb994c8
thachaprochem <thachaprochem@gmail.com>
2023-07-29 15:34:13 +0700

    change smaller fontsize to 18

--- digitalClockCopy.py
+++ digitalClockCopy.py
@@ -6,7 +6,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="green")
 
-label = Label(window, text="ThachClone29 - Newmember : Hello !", font=("Arial Black", 78, "bold"), bg="green", fg="white")
+label = Label(window, text="ThachClone29 - Newmember : Hello !", font=("Arial Black", 18, "bold"), bg="green", fg="white")
 label.pack(pady=100)
 
 window.mainloop()
