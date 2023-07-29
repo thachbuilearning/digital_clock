2764365b13c2ae224a95e203cd0dd23f8698de3c
thachaprochem <thachaprochem@gmail.com>
2023-07-29 15:24:34 +0700

    change welcome message and background color to Green

--- digitalClockCopy.py
+++ digitalClockCopy.py
@@ -4,9 +4,9 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="pink")
+window.configure(bg="green")
 
-label = Label(window, text="Welcome!", font=("Arial Black", 78, "bold"), bg="pink", fg="white")
+label = Label(window, text="ThachClone29 - Newmember : Hello !", font=("Arial Black", 78, "bold"), bg="green", fg="white")
 label.pack(pady=100)
 
 window.mainloop()
